b35ffdbddea45aff6944d2a87b5c57095c2971b4
BorisKGB <flot3.sky@yandex.ru>
2024-03-20 22:15:44 +0300

    update solution, not count next number, instead just remember prev number

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 
 int main()
 {
+    int fib_num_prev = 0;
     int fib_num = 1;
-    int fib_num_next = 1;
     int tmp;
     int count = 5;
     while (count > 0)
     {
       std::cout << fib_num << ' ';
-      tmp = fib_num;
-      fib_num = fib_num_next;
-      fib_num_next = tmp + fib_num_next;
+      tmp = fib_num_prev;
+      fib_num_prev = fib_num;
+      fib_num += tmp;
       count--;
     }
     std::cout << std::endl;
